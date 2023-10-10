52a47330ce5def9cbf0349cf88324d5c945e21e1
susoferreira <ferreirarodriguezjesus@gmail.com>
2023-10-10 17:06:59 +0200

    pissing off the linter

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ program entry point
 """
 
 def main():
-    """
-    program entry point
-    """
-    print("this line is waaaaaaaaay too long and github workflows should flag it on the PR")
 
+    print("this line is waaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaay too long and github workflows should flag it on the PR")
+
+ 
 
 if __name__ == "__main__":
     main()
