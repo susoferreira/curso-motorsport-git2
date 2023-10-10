e0f2e43a88dab142de7dc356f2e5a03f32fd70fa
susoferreira <ferreirarodriguezjesus@gmail.com>
2023-10-10 16:40:52 +0200

    Revert "made the line actually way too long"
    This reverts commit a9798efef5028dc5b04ebdcd4516cd647f6bd811.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     """
     program entry point
     """
-    print("this line is waaaaaaaaaaaaaaaaaaaaaaaaaaaaaaay too long and github workflows should flag it on the PR")
+    print("this line is waaaaaaaaay too long and github workflows should flag it on the PR")
 
 
 if __name__ == "__main__":
